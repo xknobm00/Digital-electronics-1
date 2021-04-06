82c297882a968292b5fb23e0a704dc8c41a91d90
xknobm00 <78788864+xknobm00@users.noreply.github.com>
2021-04-06 11:45:59 +0200

    xknobm00

--- Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/xsim_webtalk.tcl
+++ Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 30 15:45:20 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 30 22:08:09 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ed84ce38-5ad8-41cf-9a76-224cb6a1525e" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "25dcd2c30ab94beda7545d479ce8dbcf" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Pentium(R) CPU  N3710  @ 1.60GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.12_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6364_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 435651603 -regid "" -xml C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6340_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1197744832 -regid "" -xml C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/knoba/Desktop/de1_github/Knob/Digital-electronics-1/Labs/07-ffs/07-ffs/07-ffs.sim/sim_1/behav/xsim/xsim.dir/tb_d_latch_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
